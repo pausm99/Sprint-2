25e7b845408c55fdb944d18eb93663f509ec6805
pausm99 <pau.sabat.marti@estudiantat.upc.edu>
2023-10-10 10:22:28 +0200

    Exercici 3

--- 1.1 Arrow Functions/Nivell 1/main.js
+++ 1.1 Arrow Functions/Nivell 1/main.js
@@ -12,3 +12,17 @@ console.log(add(1, 4));
 //Exercici 2
 let randomNumber = () =>  Math.floor(Math.random() * 101);
 console.log(randomNumber());
+
+
+//---------------------
+//Exercici 3
+class Person {
+    //constructor
+    constructor(name) {
+        this.name = name;
+        this.greet = () => 'Hello ' + this.name;
+        
+    }
+}
+let pau = new Person('Pau');
+console.log(pau.greet());
