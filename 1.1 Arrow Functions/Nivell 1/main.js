3efcc03ba37ed0badd8496179349497d04b01032
pausm99 <pau.sabat.marti@estudiantat.upc.edu>
2023-10-10 10:20:50 +0200

    Exercici 2

--- 1.1 Arrow Functions/Nivell 1/main.js
+++ 1.1 Arrow Functions/Nivell 1/main.js
@@ -1,6 +1,14 @@
 // ARROW FUNCTIONS
-//---------------------
 //NIVELL 1
+
+
+//---------------------
 //Exercici 1
 let add = (a, b) =>  a + b ;
 console.log(add(1, 4));
+
+
+//---------------------
+//Exercici 2
+let randomNumber = () =>  Math.floor(Math.random() * 101);
+console.log(randomNumber());
