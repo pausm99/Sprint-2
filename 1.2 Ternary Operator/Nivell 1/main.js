f21882beb4108097a7c042a65d91835a48965c2d
pausm99 <pau.sabat.marti@estudiantat.upc.edu>
2023-10-10 10:26:59 +0200

    Exercici 2

--- 1.2 Ternary Operator/Nivell 1/main.js
+++ 1.2 Ternary Operator/Nivell 1/main.js
@@ -11,3 +11,15 @@ function potConduir(edat) {
 console.log(potConduir(24));
 console.log(potConduir(18));
 console.log(potConduir(17));
+
+
+//---------------------
+//Exercici 2
+function comparaNumeros(num1, num2) {
+    if (num1 == num2) return 'num1 i num2 són iguals';
+    return num1 > num2 ? 'num1 és més gran' : 'num2 és més gran';
+}
+
+console.log(comparaNumeros(2, 6));
+console.log(comparaNumeros(9, 4));
+console.log(comparaNumeros(5, 5));
