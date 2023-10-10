65ebe9f6ccc2dedeb66fcf1457e32eae5e81bd42
pausm99 <pau.sabat.marti@estudiantat.upc.edu>
2023-10-10 10:26:19 +0200

    Exercici 1

---
+++ 1.2 Ternary Operator/Nivell 1/main.js
@@ -0,0 +1,13 @@
+// TERNARY OPERATORS
+//NIVELL 1
+
+
+//---------------------
+//Exercici 1
+function potConduir(edat) {
+    return edat >= 18 ? 'Pots conduir' : 'No pots conduir';
+}
+
+console.log(potConduir(24));
+console.log(potConduir(18));
+console.log(potConduir(17));
