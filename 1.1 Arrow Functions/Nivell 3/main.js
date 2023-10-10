b390cd1c9ef2a5d6bfc63e13d585d0f311599b6a
pausm99 <pau.sabat.marti@estudiantat.upc.edu>
2023-10-10 10:24:22 +0200

    Exercici 5

---
+++ 1.1 Arrow Functions/Nivell 3/main.js
@@ -0,0 +1,12 @@
+// ARROW FUNCTIONS
+//---------------------
+//NIVELL 3
+//Exercici 5
+
+var printMessage = () => {
+    setTimeout(() => {
+        console.log('Missatge amb 3 segons de retard...');
+    }, 3000);
+}
+
+printMessage();
