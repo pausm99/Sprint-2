4a299f77572bbd25b00495299a62de1a07ab0a27
pausm99 <pau.sabat.marti@estudiantat.upc.edu>
2023-10-11 10:28:51 +0200

    Exercici 6

--- 1.4 Rest & Spread operators/Nivell 3/main.js
+++ 1.4 Rest & Spread operators/Nivell 3/main.js
@@ -10,3 +10,19 @@ function fn(arg1, arg2, arg3) {
 
 let array1 = [1, 2, 3];
 fn(...array1);
+
+
+//---------------------
+//Exercici 6
+let obj1 = {
+    pa: 34,
+    pb: 12
+}
+
+let obj2 = {
+    pc: 15,
+    pd: 8
+}
+
+let obj3 = {...obj1, ...obj2};
+console.log(obj3);
