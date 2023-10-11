b52b6bf8cc25580a368d3e5fc01e67f4782d008a
pausm99 <pau.sabat.marti@estudiantat.upc.edu>
2023-10-11 10:03:59 +0200

    Exercici 2

--- 1.4 Rest & Spread operators/Nivell 1/main.js
+++ 1.4 Rest & Spread operators/Nivell 1/main.js
@@ -8,3 +8,16 @@ let array1 = [1, 3, 5, 7, 9];
 let array2 = [2, 4, 6, 8, 10];
 let array3 = [...array1, ...array2];
 console.log(array3);
+
+
+//---------------------
+//Exercici 2
+function suma(...args) {
+    let suma = 0;
+    for(const arg of args) {
+        suma += arg;
+    }
+    return suma;
+}
+
+console.log(suma(12, 45, 78, 5, 9, 0, 2, 8));
