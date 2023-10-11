7eaf4fd0676b2cc46465471b5992e11b08f09e01
pausm99 <pau.sabat.marti@estudiantat.upc.edu>
2023-10-11 10:13:42 +0200

    Exercici 4

--- 1.4 Rest & Spread operators/Nivell 1/main.js
+++ 1.4 Rest & Spread operators/Nivell 1/main.js
@@ -34,3 +34,12 @@ let objecte2 = {...objecte1};
 objecte2.preu = 300;
 
 console.log(objecte1, objecte2);
+
+
+//---------------------
+//Exercici 4
+let a, b, rest;
+[a, b, ...rest] = [1, 2, 'hola', 4, 'asdas'];
+console.log(a);
+console.log(b);
+console.log(rest);
