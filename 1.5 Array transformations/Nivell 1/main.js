567277fb0390af65108ef3cbb414185798a6d758
pausm99 <pau.sabat.marti@estudiantat.upc.edu>
2023-10-11 10:33:39 +0200

    Exercici 2

--- 1.5 Array transformations/Nivell 1/main.js
+++ 1.5 Array transformations/Nivell 1/main.js
@@ -8,3 +8,11 @@ let a = [1, 2, 3, 4];
 let b = a.map(x => x*x);
 
 console.log(b);
+
+
+//---------------------
+//Exercici 2
+let c = [1, 2, 3, 4];
+let d = c.filter(x => x%2 === 0);
+
+console.log(d);
