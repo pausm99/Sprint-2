f3f31ff96996886abf3f470b1c71f5c2a681ab05
pausm99 <pau.sabat.marti@estudiantat.upc.edu>
2023-10-11 09:35:15 +0200

    Exercici 2

--- 1.3 Callbacks/Nivell 1/main.js
+++ 1.3 Callbacks/Nivell 1/main.js
@@ -13,3 +13,16 @@ function processar(nombre, fn) {
 }
 
 processar(19, callbackFunc);
+
+
+//---------------------
+//Exercici 2
+function add(n1, n2) {
+    return n1 + n2;
+}
+
+function calculadora(n1, n2, fn) {
+    console.log(fn(n1, n2));
+}
+
+calculadora(3, 8, add);
