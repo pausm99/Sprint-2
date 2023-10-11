bb51ad45941a359d09ab2d425ea5ac2568bb624a
pausm99 <pau.sabat.marti@estudiantat.upc.edu>
2023-10-11 11:43:44 +0200

    Exercici 2

--- 1.6 Array Loops/Nivell 1/main.js
+++ 1.6 Array Loops/Nivell 1/main.js
@@ -8,3 +8,10 @@ let noms = ['Anna', 'Bernat', 'Clara'];
 noms.forEach(nom => {
     console.log(nom);
 });
+
+console.log('-----------------');
+//---------------------
+//Exercici 2
+for (const nom of noms) {
+    console.log(nom);
+}
