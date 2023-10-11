0a78e4c2ee474f120176d58109c94724dda95064
Pau Sàbat Martí <83970477+pausm99@users.noreply.github.com>
2023-10-11 10:17:01 +0200

    Exercici 2
    Exercici 3 & 4 should not be here!!!

--- 1.4 Rest & Spread operators/Nivell 1/main.js
+++ 1.4 Rest & Spread operators/Nivell 1/main.js
@@ -21,25 +21,3 @@ function suma(...args) {
 }
 
 console.log(suma(12, 45, 78, 5, 9, 0, 2, 8));
-
-
-//---------------------
-//Exercici 3
-let objecte1 = {
-    nom: 'Taula',
-    preu: 250
-}
-
-let objecte2 = {...objecte1};
-objecte2.preu = 300;
-
-console.log(objecte1, objecte2);
-
-
-//---------------------
-//Exercici 4
-let a, b, rest;
-[a, b, ...rest] = [1, 2, 'hola', 4, 'asdas'];
-console.log(a);
-console.log(b);
-console.log(rest);
