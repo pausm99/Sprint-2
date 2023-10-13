2d470c64273c1ae17c1726b489183672af6fe198
pausm99 <pau.sabat.marti@estudiantat.upc.edu>
2023-10-13 20:59:09 +0200

    Exercici 3

--- 1.7 Promises & Async-Await/Nivell 1/main.js
+++ 1.7 Promises & Async-Await/Nivell 1/main.js
@@ -16,3 +16,21 @@ let promise = new Promise((resolve, reject) => {
 promise.then(result => {
     console.log(result);
 });
+
+
+//---------------------
+//Exercici 3
+const promise2 = (input) => {
+    return new Promise((resolve, reject) => {
+        if (input === 'Hola') {
+            setTimeout(() => {
+                resolve(console.log(input));
+            }, 2000);
+        }
+        else reject('Rebutjada');
+    });
+};
+
+promise2('Hola');
+promise2('Hola!');
+promise2('Adeu');
